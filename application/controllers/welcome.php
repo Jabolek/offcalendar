fb562c89eb54ad33a5967770f3a7c52805fd6990
freaky-dev <konrad.oboza@expert-reputation.com>
2014-08-19 17:49:49 +0200

    connection state handling, empty views

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -7,12 +7,53 @@ class Welcome extends CI_Controller {
 
     public function index() {
 
-        $this->load->view('master');
+        $this->data['view'] = 'calendar';
+
+        $this->load->view('master', $this->data);
     }
 
-    public function add(){
-        
+    public function notifications() {
+
+        $this->data['view'] = 'notifications';
+        $this->data['page_title'] = 'Notifications';
+
+        $this->load->view('master', $this->data);
+    }
+
+    public function past_events() {
+
+        $this->data['view'] = 'past_events';
+        $this->data['page_title'] = 'Past Events';
+
+        $this->load->view('master', $this->data);
+    }
+
+    public function your_profile() {
+
+        $this->data['view'] = 'your_profile';
+        $this->data['page_title'] = 'Your Profile';
+
+        $this->load->view('master', $this->data);
+    }
+
+    public function settings() {
+
+        $this->data['view'] = 'settings';
+        $this->data['page_title'] = 'Settings';
+
+        $this->load->view('master', $this->data);
     }
     
+    public function upcoming_events() {
+
+        $this->data['view'] = 'upcoming_events';
+        $this->data['page_title'] = 'Upcoming Events';
+
+        $this->load->view('master', $this->data);
+    }
     
+    public function add() {
+        
+    }
+
 }
--- application/views/master.php
+++ application/views/master.php
@@ -3,28 +3,33 @@
     <head>
         <meta charset=utf-8 />
         <base href="<?= base_url() ?>" />
-        <title>OffCalendar</title>
+        <title><?php if (isset($page_title)) : ?><?= $page_title . ' | ' ?><?php endif; ?>OffCalendar</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
         <link rel="stylesheet" type="text/css" media="screen" href="static/bootstrap.min.css" />
         <link rel="stylesheet" type="text/css" media="screen" href="static/bootstrap-theme.css" />
         <link rel="stylesheet" type="text/css" media="screen" href="static/bootstrap-responsive.css">
         <link rel="stylesheet" type="text/css" media="screen" href="static/style.css" />
         <link rel="stylesheet" type="text/css" media="screen" href="static/calendar.css" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <link href="static/offline-language-english-indicator.css" rel="stylesheet" type="text/css"/>
+        <link href="static/offline-language-english.css" rel="stylesheet" type="text/css"/>
+        <link href="static/offline-theme-default-indicator.css" rel="stylesheet" type="text/css"/>
+        <link href="static/offline-theme-default.css" rel="stylesheet" type="text/css"/>
     </head>
 
     <body>
         <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#">
+                    <a class="navbar-brand" href="<?= base_url() ?>">
                         <span class="logo" style="font-size: 35px;">OffCalendar</span>
                     </a>
                 </div>
 
                 <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Settings</a></li>
-                        <li><a href="#">Your Profile</a></li>
+                        <li><a href="<?= base_url() ?>welcome/settings">Settings</a></li>
+                        <li><a href="<?= base_url() ?>welcome/your_profile">Your Profile</a></li>
                         <li><a href="#">Help</a></li>
                     </ul>
                     <form class="navbar-form navbar-right">
@@ -38,56 +43,50 @@
             <div class="row">
                 <div class="col-sm-3 col-md-2 sidebar">
                     <ul class="nav nav-sidebar">
-                        <li class="active"><a href="#">Calendar</a></li>
-                        <li><a href="#">Notifications <span class="badge">3</span></a></li>
-                        <li><a href="#">Upcoming Events</a></li>
-                        <li><a href="#">Past Events</a></li>
+                        <li class="active"><a href="<?= base_url() ?>">Calendar</a></li>
+                        <li><a href="<?= base_url() ?>welcome/notifications">Notifications <span class="badge">3</span></a></li>
+                        <li><a href="<?= base_url() ?>welcome/upcoming_events">Upcoming Events</a></li>
+                        <li><a href="<?= base_url() ?>welcome/past_events">Past Events</a></li>
                         <li><a href="#">Logout</a></li>
                     </ul>
                 </div>
 
                 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                    Current state: <span class="label label-danger">OFFLINE</span>
-
                     <div class="container">
-                        <div class="page-header">
-                            <div class="pull-right form-inline">
-                                <div class="btn-group">
-                                    <button class="btn btn-primary" data-calendar-nav="prev"><< Prev</button>
-                                    <button class="btn" data-calendar-nav="today">Today</button>
-                                    <button class="btn btn-primary" data-calendar-nav="next">Next >></button>
-                                </div>
-                                <div class="btn-group">
-                                    <button class="btn btn-warning" data-calendar-view="year">Year</button>
-                                    <button class="btn btn-warning active" data-calendar-view="month">Month</button>
-                                    <button class="btn btn-warning" data-calendar-view="week">Week</button>
-                                    <button class="btn btn-warning" data-calendar-view="day">Day</button>
-                                </div>
-                            </div>
 
-                            <h3></h3>
-                        </div>
+                        <?php if (isset($view)) : ?><?php $this->load->view($view); ?><?php endif ?>
 
-                        <div class="row">
-                            <div class="span9">
-                                <div id="calendar"></div>
-                            </div>
-                        </div>
                     </div>
                 </div>
+
+                <script type="text/javascript" src="static/js/components/jquery/jquery.min.js"></script>
+                <script type="text/javascript" src="static/js/components/underscore/underscore-min.js"></script>
+                <script type="text/javascript" src="static/js/components/bootstrap2/js/bootstrap.min.js"></script>
+                <script type="text/javascript" src="static/js/components/jstimezonedetect/jstz.min.js"></script>
+
+                <script type="text/javascript" src="static/js/calendar.js"></script>
+                <script type="text/javascript" src="static/js/app.js"></script>
+
+                <script type="text/javascript" src="static/js/jquery.jeditable.js"></script>
+                <script type="text/javascript" src="static/js/OffCalendar.js"></script>
+
+                <script src="static/js/offline/offline.js" type="text/javascript"></script>
+                <script src="static/js/offline/reconnect.js" type="text/javascript"></script>
+                <script src="static/js/offline/requests.js" type="text/javascript"></script>
+                <script src="static/js/offline/simulate.js" type="text/javascript"></script>
+                <script src="static/js/offline/ui.js" type="text/javascript"></script>
+
+                <script type="text/javascript">
+                    Offline.options = {
+                        checks: {
+                            image: {
+                                url: 'http://reviewconcierge.com/static/email/blank.png?_=' + (Math.floor(Math.random() * 1000000000))
+                            },
+                            active: 'image'
+                        },
+                        reconnect: false
+                    }
+                </script>
             </div>
-
-            <script type="text/javascript" src="static/js/components/jquery/jquery.min.js"></script>
-            <script type="text/javascript" src="static/js/components/underscore/underscore-min.js"></script>
-            <script type="text/javascript" src="static/js/components/bootstrap2/js/bootstrap.min.js"></script>
-            <script type="text/javascript" src="static/js/components/jstimezonedetect/jstz.min.js"></script>
-
-            <script type="text/javascript" src="static/js/calendar.js"></script>
-            <script type="text/javascript" src="static/js/app.js"></script>
-
-            <script type="text/javascript" src="static/js/jquery.jeditable.js"></script>
-            <script type="text/javascript" src="static/js/OffCalendar.js"></script>
-
-        </div>
     </body>
 </html>
